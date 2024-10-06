484867c36f9e4e751201d57bba3bae6c6de7839d
YEvo1 <147601736+YEvo1@users.noreply.github.com>
2024-10-07 00:14:08 +0300

    Add files via upload

---
+++ Lab L33(P)/input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float num = 12.34; // Пример действительного числа
+    double exp_num = 1e10; // Экспоненциальная форма
+    printf("%f\n", num);
+    return 0;
+}
